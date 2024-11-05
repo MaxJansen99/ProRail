677d8092145a91721f9ea875598bc580fd9903ca
Max Jansen <mail@maxjansen.net>
2024-11-05 10:17:45 +0100

    Login system application

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+from routes import *
+from models import *
+
+with app.app_context():
+  db.create_all()
+
+if __name__ == "__main__":
+  app.run(debug=True)
---
+++ Flask/routes.py
@@ -0,0 +1,44 @@
+from flask import render_template, redirect, url_for, flash
+from app import app, db
+from models import User
+from forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for("home"))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash("Your account has been created!", "success")
+    return redirect(url_for("login"))
+  return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for("home"))
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user and user.check_password(form.password.data):
+      login_user(user)
+      return redirect(url_for("home"))
+    else:
+      flash("Login Unsuccessful. Please check email and password", "danger")
+  return render_template("login.html", form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html')
