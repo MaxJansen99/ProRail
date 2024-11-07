5a564b3c66575e47cdf02d89fffdf9e67c9e0871
Max Jansen <mail@maxjansen.net>
2024-11-07 01:01:34 +0100

    Merging Front-end & Back-end of application

--- Flask/forms.py
+++ Flask/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-  firstname = StringField('Firstname', validators=[DataRequired(), Length(min=2, max=20)])
-  lastname = StringField('Lastname', validators=[DataRequired(), Length(min=2, max=20)])
+  firstname = StringField('Voornaam', validators=[DataRequired(), Length(min=2, max=20)])
+  lastname = StringField('Achternaam', validators=[DataRequired(), Length(min=2, max=20)])
   email = StringField('Email', validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-  submit = SubmitField('Sign Up')
+  password = PasswordField('Wachtwoord', validators=[DataRequired()])
+  confirm_password = PasswordField('Bevestig Wachtwoord', validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField('Registreren')
 
 class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  submit = SubmitField('Login')
+  password = PasswordField('Wachtwoord', validators=[DataRequired()])
+  submit = SubmitField('Inloggen')
--- Flask/routes.py
+++ Flask/routes.py
@@ -7,7 +7,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/register", methods=["GET", "POST"])
 def register():
   if current_user.is_authenticated:
-    return redirect(url_for("home"))
+    return redirect(url_for("dashboard"))
   form = RegistrationForm()
   if form.validate_on_submit():
     user = User(firstname=form.firstname.data, lastname=form.lastname.data, email=form.email.data)
@@ -21,13 +21,13 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
   if current_user.is_authenticated:
-    return redirect(url_for("home"))
+    return redirect(url_for("dashboard"))
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(email=form.email.data).first()
     if user and user.check_password(form.password.data):
       login_user(user)
-      return redirect(url_for("home"))
+      return redirect(url_for("dashboard"))
     else:
       flash("Login Unsuccessful. Please check email and password", "danger")
   return render_template("login.html", form=form)
@@ -36,9 +36,9 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 @app.route('/')
 @login_required
-def home():
-    return render_template('home.html')
+def dashboard():
+    return render_template('dashboard.html')
